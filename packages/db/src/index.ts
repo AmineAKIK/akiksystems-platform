722153d3bb4f6e7a879ebe83644c981a952c6b26
Mohamed Amine AKIK <223266361+AmineAKIK@users.noreply.github.com>
2026-09-26 01:32:13 +0200

    Export Work with us inquiry row type

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -298,6 +298,7 @@ export type {
   WorkWithUsSystemsTable,
   WorkWithUsSystemUpdate,
   NewProfilePublicationRow,
+  NewWorkWithUsInquiryRow,
   NewWorkWithUsLocalizationRow,
   NewWorkWithUsPageRow,
   NewWorkWithUsPublicationRow,
